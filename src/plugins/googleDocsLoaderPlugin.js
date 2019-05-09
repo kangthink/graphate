97ad2eb25eae78c83c3fc7d49bd1fc633d86f6f2
tae hoon kang <kangthink@gmail.com>
2019-05-09 18:03:55 +0900

    Adding tests for googleDocsLoaderPlugin

--- src/plugins/googleDocsLoaderPlugin.js
+++ src/plugins/googleDocsLoaderPlugin.js
@@ -1,1 +1,13 @@
 // googleDocsLoaderPlugin.js
+
+function GoogleDocsLoaderPlugin(options) {
+  this.options = options
+}
+
+GoogleDocsLoaderPlugin.prototype.apply = function(ctx) {
+  ctx.data = 'loaded data!!'
+}
+
+module.exports = {
+  GoogleDocsLoaderPlugin
+}
--- src/plugins/grammarLoaderPlugin.js
+++ src/plugins/grammarLoaderPlugin.js
@@ -1,25 +1,25 @@
 // grammarLoaderPlugin.js
 
-function GrammarLoaderPlugin(option) {
-    this.option = option
-  }
+function GrammarLoaderPlugin(options) {
+  this.options = options
+}
   
-  GrammarLoaderPlugin.prototype.apply = function(ctx) {
-  
-    const nodes = [
-      { id: '가정법', data: { in: 5 } },
-      { id: '가정법 과거', data: { in: 0 } },
-      { id: '가정법 과거완료', data: { in: 0 } },
-    ]
-  
-    const edges = [
-      ['가정법 과거', '가정법', 4],
-      ['가정법 과거완료', '가정법', 1],
-    ]
-  
-    ctx.data = { nodes, edges }
-  }
-  
-  module.exports = {
-    GrammarLoaderPlugin
-  }
+GrammarLoaderPlugin.prototype.apply = function(ctx) {
+
+  const nodes = [
+    { id: '가정법', data: { in: 5 } },
+    { id: '가정법 과거', data: { in: 0 } },
+    { id: '가정법 과거완료', data: { in: 0 } },
+  ]
+
+  const edges = [
+    ['가정법 과거', '가정법', 4],
+    ['가정법 과거완료', '가정법', 1],
+  ]
+
+  ctx.data = { nodes, edges }
+}
+
+module.exports = {
+  GrammarLoaderPlugin
+}
